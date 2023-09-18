97a016f6af1e87f06b653fc69198c8f2ed387f1b
Dilip Suthar <devdilip@protonmail.ch>
2023-09-18 15:52:36 +0530

    final

--- src/components/DigitalClock.jsx
+++ src/components/DigitalClock.jsx
@@ -3,7 +3,7 @@ import { AlarmContext } from '../context/Alarm';
 import './digitalClock.css';
 
 const DigitalClock = () => {
-  const { hourDigital, minutesDigital, amPm, dayNow, monthNow, yearNow } =
+  const { hourDigital, minutesDigital,secondDigital, amPm, dayNow, monthNow, yearNow,timeline} =
     useContext(AlarmContext);
 
   return (
@@ -11,12 +11,18 @@ const DigitalClock = () => {
       <div className="clock-text">
         <div className="clock-text-hour">{`${hourDigital}:`}</div>
         <div className="clock-text-minutes">{minutesDigital}</div>
+        <div className="clock-text-minutes">{`:${secondDigital}`}</div>
         <div className="clock-text-ampm">{amPm}</div>
       </div>
       <div className="clock-date">
         <span>{`${dayNow} `}</span>
         <span>{`${monthNow} , `}</span>
+        
         <span>{yearNow}</span>
+        <br/>
+        <span>{timeline}</span>
+         
+        
       </div>
     </div>
   );
--- src/context/Alarm.jsx
+++ src/context/Alarm.jsx
@@ -1,20 +1,49 @@
 import React, { createContext, useEffect, useState } from 'react';
 import months from '../constants/months';
-import Sound from '../assets/mixkit-casino-win-alarm-and-coins-1990.mp3';
-
-const alarm = new Audio(Sound);
+import Sound from '../assets/ring.mp3';
+import loud from  "../assets/loudring.mp3";
+const notifi = new Audio(Sound);
+const ring = new Audio(loud);
 export const AlarmContext = createContext();
-
+const getData=(setdata,link)=>{
+  fetch(link
+  ,{
+    headers : { 
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+     }
+  }
+  )
+    .then(function(response){
+      
+      return response.json();
+    })
+    .then(function(myJson) {
+      
+      setdata(myJson)
+    });
+}
 function Alarm({ children }) {
   const [hourDigital, setHourDigital] = useState('');
   const [minutesDigital, setMinutesDigital] = useState('');
+  const [secondDigital, setSecondDigital] = useState('');
   const [amPm, setAmPm] = useState('');
   const [dayNow, setDayNow] = useState('');
   const [monthNow, setMonthNow] = useState('');
   const [yearNow, setYearNow] = useState('');
   const [alarmTime, setAlarmTime] = useState('');
   const [hasAlarm, setHasAlarm] = useState(false);
+  const [data,setdata] = useState({})
+  const [timeline,settimeline] = useState("")
+  
+  getData(setdata,"https://api.npoint.io/7cdea25f07d61d8d3e5d")
 
+  if(timeline !=  ""){
+    setTimeout(() => {
+      settimeline("")
+    }, 10000);
+  }
+  
   useEffect(() => {
     setInterval(() => {
       let date = new Date();
@@ -23,6 +52,7 @@ function Alarm({ children }) {
       let day = date.getDate();
       let month = date.getMonth();
       let year = date.getFullYear();
+      let sec= date.getSeconds()
       let ampm;
 
       if (HH >= 12) {
@@ -34,38 +64,43 @@ function Alarm({ children }) {
       if (HH === 0) HH = 12;
       if (HH < 10) HH = `0${HH}`;
       if (MM < 10) MM = `0${MM}`;
-
+      if (sec < 10) sec = `0${sec}`;
+      setSecondDigital(sec);
       setHourDigital(HH);
       setMinutesDigital(MM);
       setAmPm(ampm);
       setDayNow(day);
+
       setMonthNow(months[month]);
       setYearNow(year);
     }, 1000);
   }, []);
-
-  if (alarmTime === `${hourDigital}:${minutesDigital} ${amPm}`) {
-    alarm.play();
-    alarm.loop = true;
+  var time = `${hourDigital}:${minutesDigital}:${secondDigital} ${amPm}`
+  if (Object.keys(data).includes(time)) {
+   
+    if(data[time].split("_")[1] == "s"){
+      ring.play();
+      ring.loop();
+    }else{
+      notifi.play();
+      notifi.play();
+    }
+    if(timeline == ""){
+      settimeline(data[time].split("_")[0])
+    }
+    
   }
-  const pauseAlarm = () => {
-    alarm.pause();
-    setAlarmTime('');
-  };
   return (
     <AlarmContext.Provider
       value={{
         hourDigital,
         minutesDigital,
+        secondDigital,
         amPm,
         dayNow,
         monthNow,
         yearNow,
-        alarmTime,
-        setAlarmTime,
-        pauseAlarm,
-        hasAlarm,
-        setHasAlarm,
+        timeline
       }}
     >
       {children}
